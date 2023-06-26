2f99040ed5e3d4e534edaf059438e8d9b90100c7
Melissa Eberhart <137014844+theseaisdark@users.noreply.github.com>
2023-06-25 22:46:40 -0700

    latest update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,151 @@
 // STEP 1
+
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
 // STEP 2
+
+// let gardens = 2;
+// let starwars = 'Ewoks are adorable!';
+// let starwarsIsAwesome = true;
+// let giftcard = null;
+
 // STEP 3
+
+// let itsComplicated = 4 + 6;
+// console.log(itsComplicated);
+
+// let itsSubtraction = 6 - 4;
+// console.log(itsSubtraction)
+
+
 // STEP 4
+
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let nZipCode;
+// let uAge;
+// let referralSource;
+// let ynContact;
+
+
 // STEP 5
+
+// let state;
+// let unitedStates;
+// state = 'California';
+// unitedStates = true;
+
+// const nZipCode = 91941;
+// alert(nZipCode);
+
+// let uAge = '39';
+// alert('Melissa is ' + uAge + ' years old.');
+
+
 // STEP 6
+
+// let yearsPainting = 20;
+// console.log('Melissa has been painting for ' + yearsPainting + ' years.');
+
 // STEP 7
+
+// let x = 7;
+// let y = 83;
+// console.log (x == 7 && y == 83);
+
 // STEP 8
+
+//The string is not valid. Need to escape the apostrophe like this:
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
 // STEP 9
+
+// let x = 717;
+// x = null;
+// console.log(x);
+
+// let meowMeow;
+// console.log(meowMeow);
+
 // STEP 10
+
+
 // STEP 11
+
+// let x = 'Melissa'
+// let y = 'Eberhart'
+// alert('Hello ' + x + ' ' + y + ', welcome to the Javascript class!');
+
 // STEP 12
+
+// let name = 'Melissa';
+// alert('Hello name, welcome to the Javascript class!');
+
 // STEP 13
+
+// let course = 'Javascript';
+// let firstName = 'Melissa' 
+// let lastName = 'Eberhart'
+// alert('Hello ' + firstName + ' ' + lastName + ', welcome to the ' + course + ' class!');
+
 // STEP 14
+
+// let course = 'Javascript';
+// let firstName = 'Melissa' 
+// let lastName = 'Eberhart'
+// alert('Hello ' + firstName + ' ' + lastName + '. \n Welcome to the ' + course + ' class!');
+
+
 // STEP 15
+
+// let studentName = prompt('What\'s your name?');
+// let course = 'Javascript';
+// alert('Hello ' + studentName + '! \nWelcome to the ' + course + ' class!');
+
 // STEP 16
+
+// let studentName = prompt('What\'s your name?');
+// let course = prompt('What course are you taking?');
+// alert('Hello ' + studentName + '! \nWelcome to the ' + course + ' class!');
+
 // STEP 17
+
+// let x = 10
+// let y = 20
+// let sum = x + y;
+// console.log('The sum of ' + x + ' and ' + y + ' is: ' + sum );
+
 // STEP 18
+
+// let x = 20
+// console.log(x += 20);
+
 // STEP 19
+
+// let x = 20
+// console.log(x *= 5);
+
 // STEP 20
+
+// let x = 20% 3;
+// console.log(x /= 1);
+
 // STEP 21
+
+// let x = 777;
+// let y = 777;
+// console.log(x == y);
+
 // STEP 22
+
+let x = 8;
+let y = 29;
+console.log(x > y);
